eaf8fd981659d272d665799a678db39f859247b3
1mt3j45 <agenttj.rawal98@gmail.com>
2018-03-11 21:24:47 +0530

    Comparative Graph Generation

--- plotter.py
+++ plotter.py
@@ -84,3 +84,98 @@ def uslplt(groundTruth, predictedValues, plt_name="<name>"):
 
     plt.tight_layout()
     plt.show()
+
+
+def stackplotter(HighlyNEG, ModeratelyNEG, NEG, HighlyPOS, ModeratelyPOS, POS, text):
+    H_NG, M_NG, NG, H_PS, M_PS, PS = HighlyNEG, ModeratelyNEG, NEG, HighlyPOS, ModeratelyPOS, POS
+    Polarities = [H_NG, M_NG, NG, H_PS, M_PS, PS]
+    N = len(Polarities)  # Number of Bars
+
+    ind = np.arange(N)  # the x locations for the groups
+    width = 0.35  # the width of the bars: can also be len(x) sequence
+
+    p1 = plt.bar(ind, Polarities, width)
+
+    plt.xlabel('Polarities')
+    plt.ylabel('Scores')
+    plt.title(text)
+    plt.xticks(ind, (
+    'Highly Negative', 'Moderately Negative', 'Negative', 'Highly Positive', 'Moderately Positive', 'Positive'))
+    # plt.yticks(np.arange(0, 200, 10))
+    # plt.legend(p1[0], 'Polarity')
+
+    plt.show()
+
+def stackplotter3d(HighlyNEG, ModeratelyNEG, NEG, HighlyPOS, ModeratelyPOS, POS, text):
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection="3d")
+    H_NG, M_NG, NG, H_PS, M_PS, PS = HighlyNEG, ModeratelyNEG, NEG, HighlyPOS, ModeratelyPOS, POS
+    Polarities = [H_NG, M_NG, NG, H_PS, M_PS, PS]
+    # N = len(Polarities)  # Number of Bars
+
+    # ind = np.arange(N)  # the x locations for the groups
+    # width = 0.35  # the width of the bars: can also be len(x) sequence
+
+    #p1 = plt.bar(ind, Polarities, width)
+
+    ax.set_xlabel('Polarities')
+    ax.set_ylabel('Scores')
+    ax.set_zlabel('z')
+    #ax.title("TESTING")
+
+    ax.set_xlim3d(0, 14)
+    ax.set_ylim3d(0, 4)
+
+    #ax.xticks(ind, ('Highly Negative', 'Moderately Negative', 'Negative', 'Highly Positive', 'Moderately Positive', 'Positive'))
+
+    xpos = [2, 4, 6, 8, 10, 12]
+    ypos = [1, 1, 1, 1, 1, 1]
+    zpos = Polarities
+
+    dx = np.ones(6)
+    dy = np.ones(6)
+    dz = [np.random.random(6) for i in range(4)]# the heights of the 4 bar sets
+
+    _zpos = zpos  # the starting zpos for each bar
+
+    for i in range(len(dz)):
+        ax.bar3d(xpos, ypos, _zpos, dx, dy, dz[i], color='red')
+        _zpos += dz[i]  # add the height of each bar to know where to start the next
+
+    plt.gca().invert_xaxis()
+
+    # plt.yticks(np.arange(0, 200, 10))
+    # plt.legend(p1[0], 'Polarity')
+
+    plt.show()
+
+    # # ------------------------------------------------------------
+# from mpl_toolkits.mplot3d import Axes3D
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection="3d")
+#
+# ax.set_xlabel("x")
+# ax.set_ylabel("y")
+# ax.set_zlabel("z")
+# ax.set_xlim3d(0, 10)
+# ax.set_ylim3d(0, 10)
+#
+# xpos = [2, 5, 8, 2, 5, 8, 2, 5, 8]
+# ypos = [1, 1, 1, 5, 5, 5, 9, 9, 9]
+# zpos = np.zeros(9)
+#
+# dx = np.ones(9)
+# dy = np.ones(9)
+# dz = [np.random.random(9) for i in range(4)]  # the heights of the 4 bar sets
+#
+# _zpos = zpos  # the starting zpos for each bar
+# colors = ['r', 'b', 'g', 'y', 'r', 'b', 'y']
+# for i in range(4):
+#     ax.bar3d(xpos, ypos, _zpos, dx, dy, dz[i], color=colors[i])
+#     _zpos += dz[i]  # add the height of each bar to know where to start the next
+#
+# plt.gca().invert_xaxis()
+# plt.show()
